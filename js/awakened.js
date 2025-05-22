3afd038bc48cf1acbbca4629057f8056d243b5e0
Laptop\James <james.fierens@outlook.fr>
2025-05-22 15:59:42 +0200

    presenation

--- js/awakened.js
+++ js/awakened.js
@@ -4,9 +4,11 @@ const errorMsg = document.getElementById("errorMsg");
 
 form.addEventListener("submit", function (e) {
     e.preventDefault();
-    const u = input.value.trim();
+    const u = input.value.trim()
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "");
     const k = btoa(u);
-    const x = "TEBfVjNyaXTDqQ==";
+    const x = "TEBfVjNyaXRl";
     if (k === x) {
         const t = atob(form.getAttribute("data-key"));
         window.location.href = t;
